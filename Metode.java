59940c552153454b2b264797c38ca716dd27b72d
Mladen Ristic <mladenristic1997@gmail.com>
2017-12-08 14:50:37 +0100

    dodano sabiranje i oduzimanje

--- Metode.java
+++ Metode.java
@@ -3,13 +3,13 @@ package kalkulator;
 public class Metode {
 	//Mladen
 	public static double sabiranje(double a, double b) {
-		
-		return nesto;
+			
+		return a + b;
 	}
 	//Mladen
 	public static double oduzimanje(double a, double b) {
 			
-			return nesto;
+			return a - b;
 		}
 	//Eldin
 	public static double mnozenje(double a, double b) {
@@ -22,19 +22,20 @@ public class Metode {
 		return nesto;
 	}
 	//Stefan
-	public static double korjenovanje(double a, double b) {
+	public static double korjenovanje(double a) {
 		
 		return nesto;
 	}
 	//Stefan
-	public static double kvadriranje(double a, double b) {
+	public static double kvadriranje(double a) {
 		
 		return nesto;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println(sabiranje(2, 3));
+		System.out.println(oduzimanje(3, 6));
 	}
 
 }
