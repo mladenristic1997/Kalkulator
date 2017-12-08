ab6262c9d8bfdaacd5f5a98dc2a58058b5bf9237
Mladen Ristic <mladenristic1997@gmail.com>
2017-12-08 14:31:27 +0100

    kostur aplikacije

---
+++ Metode.java
@@ -0,0 +1,40 @@
+package kalkulator;
+
+public class Metode {
+	//Mladen
+	public static double sabiranje(double a, double b) {
+		
+		return nesto;
+	}
+	//Mladen
+	public static double oduzimanje(double a, double b) {
+			
+			return nesto;
+		}
+	//Eldin
+	public static double mnozenje(double a, double b) {
+		
+		return nesto;
+	}
+	//Eldin
+	public static double dijeljenje(double a, double b) {
+		
+		return nesto;
+	}
+	//Stefan
+	public static double korjenovanje(double a, double b) {
+		
+		return nesto;
+	}
+	//Stefan
+	public static double kvadriranje(double a, double b) {
+		
+		return nesto;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
